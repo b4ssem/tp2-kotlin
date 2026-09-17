47bfee953369d3325a3d4da341440adb39d3601f
Bassem Meghiche <bmeghiche06@gmail.com>
2026-09-17 09:36:07 +0200

    Exercice 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
 fun main() {
-
+    // Consigne 1
+    val playlist: MutableList<String> = mutableListOf("Bohemian Rhapsody", "Stairway to Heaven", "Hotel California")
+    // Consigne 2
+    playlist.add("Imagine")
+    playlist.add(0, "Like a Rolling Stone")
+    playlist.add("Bohemian Rhapsody")
+    // Consigne 3
+    playlist.set(2, "Smells Like Teen Spirit")
+    // Consigne 4
+    println("Hotel California" in playlist)
+    println(playlist.indexOf("Imagine"))
+    // Consigne 5
+    playlist.remove("Hotel California")
+    playlist.remove(playlist[0])
+    // Consigne 6
+    playlist.sort()
+    println(playlist)
+    playlist.sortDescending()
+    println(playlist)
 }
